496a950d900fc712b545dc876f91e1540284e1b8
Swetank Subham <39980452+swtnk@users.noreply.github.com>
2024-12-24 18:17:48 +0530

    Added Book Reader

---
+++ assets/js/app.js
@@ -0,0 +1,47 @@
+const url = '/assets/books/the_curse_of_avelorn_v1.pdf';
+
+//
+// The workerSrc property shall be specified.
+//
+pdfjsLib.GlobalWorkerOptions.workerSrc = '/assets/js/pdf.worker.js';
+
+//
+// Asynchronous download PDF
+//
+const loadingTask = pdfjsLib.getDocument(url);
+(async () => {
+const pdf = await loadingTask.promise;
+//
+// Fetch the first page
+//
+const page = await pdf.getPage(1);
+const scale = 1.5;
+const viewport = page.getViewport({ scale });
+// Support HiDPI-screens.
+const outputScale = window.devicePixelRatio || 1;
+
+//
+// Prepare canvas using PDF page dimensions
+//
+const canvas = document.getElementById("book-reader");
+const context = canvas.getContext("2d");
+
+canvas.width = Math.floor(viewport.width * outputScale);
+canvas.height = Math.floor(viewport.height * outputScale);
+// canvas.style.width = Math.floor(viewport.width) + "px";
+// canvas.style.height = Math.floor(viewport.height) + "px";
+
+const transform = outputScale !== 1 
+    ? [outputScale, 0, 0, outputScale, 0, 0] 
+    : null;
+
+//
+// Render PDF page into canvas context
+//
+const renderContext = {
+    canvasContext: context,
+    transform,
+    viewport,
+};
+page.render(renderContext);
+})();
